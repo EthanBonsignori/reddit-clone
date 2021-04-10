9806a785635736ca6d65ecaa764d6f997b50b9eb
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-09 23:25:52 -0400

    register + login mutations

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,22 +1,25 @@
-import { Resolver, Mutation, InputType, Field, Arg, } from 'type-graphql'
-import { User } from 'src/entities/User';
+import { Resolver, Mutation, InputType, Field, Arg, ObjectType, } from 'type-graphql'
+import argon2 from 'argon2';
+import { User } from '../entities/User';
+// import { MyContext } from 'src/types';
 
-// @ObjectType()
-// class FieldError {
-//   @Field()
-//   field: string;
-//   @Field()
-//   message: string;
-// }
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
 
-// @ObjectType()
-// class UserResponse {
-//   @Field(() => [FieldError], { nullable: true })
-//   errors?: FieldError[];
+  @Field()
+  message: string;
+}
 
-//   @Field(() => User, { nullable: true })
-//   user?: User;
-// }
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
 
 @InputType()
 class UsernamePasswordInput {
@@ -29,15 +32,50 @@ class UsernamePasswordInput {
 
 @Resolver()
 export class UserResolver {
-  @Mutation(() => String)
-  register(
-    @Arg('options') options: UsernamePasswordInput
+
+  @Mutation(() => User)
+  async register(
+    @Arg('options') options: UsernamePasswordInput,
+    @Arg('password') password: string,
     // @Ctx() { }: MyContext
   ): Promise<User> {
+    const hashedPassword = await argon2.hash(password)
     return User.create({
       username: options.username,
       // email: options.email,
-      password: options.password,
+      password: hashedPassword,
     }).save();
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg('options') options: UsernamePasswordInput,
+    @Arg('password') password: string,
+    // @Ctx() { req }: MyContext,
+  ): Promise<UserResponse> {
+    const user = await User.findOne({ username: options.username })/*  */
+    if (!user) {
+      return {
+        errors: [{
+          field: 'username',
+          message: 'That user doesn\'t exist'
+        }]
+      }
+    }
+    const validPassword = await argon2.verify(user.password, password)
+    if (!validPassword) {
+      return {
+        errors: [{
+          field: 'password',
+          message: 'Incorrect Password',
+        }]
+      }
+    }
+
+    // req.session.userId = user.id;
+
+    return {
+      user,
+    };
   }
 }
