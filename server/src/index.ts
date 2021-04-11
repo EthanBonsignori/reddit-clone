7b6535bf4f3e0c3ad2a0b75f93cecda1ca8e0a3d
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-10 23:19:02 -0400

    add sessions and cookie

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,12 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm'
+import redis from 'redis';
+import connectRedis from 'connect-redis'
 import express from 'express';
+import session from 'express-session';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
+import { MyContext } from './types'
 import { __prod__ } from './constants';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
@@ -12,13 +16,34 @@ const main = async () => {
   await createConnection();
 
   const app = express();
+  const RedisStore = connectRedis(session)
+  const redisClient = redis.createClient()
+
+  app.use(
+    session({
+      name: 'sid',
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
+        httpOnly: true,
+        sameSite: 'lax',
+        secure: __prod__,
+      },
+      saveUninitialized: false,
+      secret: 'aosdijaosidjasodij',
+      resave: false,
+    })
+  )
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => { return }
+    context: ({ req, res }): MyContext => ({ req, res })
   })
 
   apolloServer.applyMiddleware({ app })
--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -1,6 +1,7 @@
-import { Resolver, Mutation, InputType, Field, Arg, ObjectType, } from 'type-graphql'
+import { Resolver, Mutation, InputType, Field, Arg, ObjectType, Ctx, Query, } from 'type-graphql'
 import argon2 from 'argon2';
 import { User } from '../entities/User';
+import { MyContext } from 'src/types';
 // import { MyContext } from 'src/types';
 
 @ObjectType()
@@ -32,28 +33,75 @@ class UsernamePasswordInput {
 
 @Resolver()
 export class UserResolver {
+  @Query(() => User, { nullable: true })
+  async me(
+    @Ctx() { req }: MyContext
+  ) {
+    // User not logged in
+    if (!req.session.userId) {
+      return null
+    };
 
-  @Mutation(() => User)
+    const user = await User.findOne({ id: req.session.userId })
+    return user;
+  }
+
+  @Mutation(() => UserResponse)
   async register(
-    @Arg('options') options: UsernamePasswordInput,
-    @Arg('password') password: string,
+    @Arg('options') options: UsernamePasswordInput
+    // @Arg('password') password: string,
     // @Ctx() { }: MyContext
-  ): Promise<User> {
-    const hashedPassword = await argon2.hash(password)
-    return User.create({
-      username: options.username,
-      // email: options.email,
-      password: hashedPassword,
-    }).save();
+  ): Promise<UserResponse> {
+
+    if (options.username.length <= 2) {
+      return {
+        errors: [{
+          field: 'username',
+          message: 'Username must be 3 or more characters'
+        }]
+      }
+    }
+
+    if (options.password.length < 5) {
+      return {
+        errors: [{
+          field: 'password',
+          message: 'Password must be 5 or more characters'
+        }]
+      }
+    }
+
+    const hashedPassword = await argon2.hash(options.password)
+    let user;
+    try {
+      user = await User.create({
+        username: options.username,
+        // email: options.email,
+        password: hashedPassword,
+      }).save();
+    } catch (err) {
+      if (err?.code === '23505' || err?.detail.includes('already exists')) {
+        return {
+          errors: [{
+            field: 'username',
+            message: 'Username already taken'
+          }]
+        }
+      }
+    }
+
+    return {
+      user,
+    }
   }
 
   @Mutation(() => UserResponse)
   async login(
     @Arg('options') options: UsernamePasswordInput,
-    @Arg('password') password: string,
-    // @Ctx() { req }: MyContext,
+    // @Arg('password') password: string,
+    @Ctx() { req }: MyContext,
   ): Promise<UserResponse> {
-    const user = await User.findOne({ username: options.username })/*  */
+    const user = await User.findOne({ username: options.username })
     if (!user) {
       return {
         errors: [{
@@ -62,17 +110,17 @@ export class UserResolver {
         }]
       }
     }
-    const validPassword = await argon2.verify(user.password, password)
+    const validPassword = await argon2.verify(user.password, options.password)
     if (!validPassword) {
       return {
         errors: [{
           field: 'password',
-          message: 'Incorrect Password',
+          message: 'Incorrect password',
         }]
       }
     }
 
-    // req.session.userId = user.id;
+    req.session.userId = user.id;
 
     return {
       user,
