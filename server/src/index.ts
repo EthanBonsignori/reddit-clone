3181966139554e6cac338f1af52528b0ba20503d
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-12 18:54:41 -0400

    add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import redis from 'redis';
 import connectRedis from 'connect-redis'
 import express from 'express';
 import session from 'express-session';
+import cors from 'cors';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { MyContext } from './types'
@@ -18,6 +19,11 @@ const main = async () => {
   const app = express();
   const RedisStore = connectRedis(session)
   const redisClient = redis.createClient()
+
+  app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  }))
 
   app.use(
     session({
@@ -46,7 +52,10 @@ const main = async () => {
     context: ({ req, res }): MyContext => ({ req, res })
   })
 
-  apolloServer.applyMiddleware({ app })
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  })
 
   app.listen(4000, () => {
     console.log('server started on port 4000')
