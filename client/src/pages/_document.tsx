d604bf226d809faa07c1551e5a032d78e8dbe59a
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-15 16:32:50 -0400

    linting errors

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,18 +1,23 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { ColorModeScript } from '@chakra-ui/react'
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import { ColorModeScript } from '@chakra-ui/react';
+import theme from '../theme';
 
-export default class Document extends NextDocument {
+
+class Document extends NextDocument {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   render() {
     return (
       <Html>
         <Head />
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
-          <ColorModeScript />
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
+
+export default Document;
