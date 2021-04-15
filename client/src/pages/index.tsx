d7e9b70f85dce707e4005760c770bbd495667261
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-15 16:33:58 -0400

    add layout

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,24 @@
-const Index = () => (
-  <div>Hello World</div>
-)
+import { Stack } from '@chakra-ui/layout';
+import { useColorModeValue } from '@chakra-ui/color-mode';
+import { Navbar } from '../components/Navbar';
 
-export default Index
+const Index: React.FC = () => {
+  const color = useColorModeValue('lightText', 'darkText');
+  const bg = useColorModeValue('lightBg', 'darkBg');
+
+  return (
+    <Stack
+      as='main'
+      align='center'
+      bg={bg}
+      color={color}
+      minH='calc(100vh - 48px)'
+      mt='48px'
+    >
+      <Navbar color={color} />
+      <div>Hello World</div>
+    </Stack>
+  );
+};
+
+export default Index;
