03f081d107c8b090d0f60fa400b5c168446556a4
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-15 16:16:55 -0400

    add theme toggle

---
+++ client/src/components/ColorModeSwitch.tsx
@@ -0,0 +1,15 @@
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { useColorMode, IconButton } from '@chakra-ui/react';
+
+const ColorModeSwitch: React.FC = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <IconButton
+      aria-label="Toggle Color Mode Switch"
+      icon={colorMode === 'dark' ? <MoonIcon /> : <SunIcon />}
+      onClick={toggleColorMode}
+    />
+  );
+};
+
+export default ColorModeSwitch;
