012a5c9e11d446ee4c2165db4475779c7444c440
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-16 11:28:36 -0400

    create main button

---
+++ client/src/components/MainButton.tsx
@@ -0,0 +1,39 @@
+import { Button } from '@chakra-ui/button';
+
+interface MainButtonProps {
+  text: string,
+  color: string,
+  bg: string,
+  border: string,
+  marginLeft?: string,
+}
+
+const MainButton: React.FC<MainButtonProps> = ({ text, color, bg, border, marginLeft }) => {
+  return (
+    <Button
+      as='a'
+      cursor='pointer'
+      fontFamily='Noto Sans, Arial, sans-serif'
+      fontWeight='700'
+      lineHeight='17px'
+      padding='4px 16px'
+      h='34px'
+      minW='32px'
+      width='auto'
+      marginLeft={marginLeft || '0'}
+      fontSize='14px'
+      color={color}
+      bg={bg}
+      border='1px solid'
+      borderColor={border}
+      borderRadius='9999px'
+      _hover={{
+        opacity: '0.8',
+      }}
+    >
+      {text}
+    </Button>
+  );
+};
+
+export default MainButton;
