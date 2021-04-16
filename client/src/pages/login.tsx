b22f9996ec3e45bc455643debe3b23c339b26c56
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-16 11:26:36 -0400

    fix submit func

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,36 +1,29 @@
-import React from 'react'
 import { useRouter } from 'next/router';
-import { Form, Formik } from 'formik'
+import { Form, Formik } from 'formik';
 import { Box, Button } from '@chakra-ui/react';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
 import { useLoginMutation } from '../generated/graphql';
 import { toErrorMap } from '../utils/toErrorMap';
 
-interface registerProps {
-
-}
-
-const Login: React.FC<registerProps> = ({ }) => {
+const Login: React.FC = () => {
   const router = useRouter();
-  const [{}, login] = useLoginMutation();
-
-  const handleSubmit = async (values: any, { setErrors }: any) => {
-    const response = await login(values);
-    console.log(response);
-    if (response.data?.login.errors) {
-      setErrors(toErrorMap(response.data.login.errors));
-    } else if (response.data?.login.user) {
-      // Register successful
-      router.push('/');
-    }
-  }
+  const [, login] = useLoginMutation();
 
   return (
     <Wrapper size='small'>
       <Formik
-        initialValues={{ username: '', password: '', }}
-        onSubmit={handleSubmit}
+        initialValues={{ username: '', password: '' }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login({ options: values });
+          console.log(response);
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data.login.errors));
+          } else if (response.data?.login.user) {
+            // Register successful
+            router.push('/');
+          }
+        }}
       >
         {({ isSubmitting }) => (
           <Form>
@@ -53,6 +46,6 @@ const Login: React.FC<registerProps> = ({ }) => {
       </Formik>
     </Wrapper>
   );
-}
+};
 
 export default Login;
