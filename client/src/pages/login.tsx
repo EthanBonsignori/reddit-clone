94522acb3dc65c85a07021e5aedcc19ad5fef235
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-16 23:09:40 -0400

    attempt to strongly type submit function

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,30 +1,35 @@
 import { useRouter } from 'next/router';
-import { Form, Formik } from 'formik';
 import { Box, Button } from '@chakra-ui/react';
+import { Form, Formik, FormikHelpers, FormikValues } from 'formik';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
-import { useLoginMutation } from '../generated/graphql';
+import { UsernamePasswordInput, useLoginMutation } from '../generated/graphql';
 import { toErrorMap } from '../utils/toErrorMap';
 
 const Login: React.FC = () => {
   const router = useRouter();
   const [, login] = useLoginMutation();
 
+  const handleSubmit = async (
+    values: UsernamePasswordInput,
+    { setErrors }: FormikHelpers<FormikValues>,
+  ) => {
+    const response = await login({ options: values });
+    console.log(response);
+    if (response.data?.login.errors) {
+      return setErrors(toErrorMap(response.data.login.errors));
+    } else if (response.data?.login.user) {
+      // Register successful
+      return router.push('/');
+    }
+    return;
+  };
+
   return (
     <Wrapper size='small'>
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={async (values, { setErrors }) => {
-          const response = await login({ options: values });
-          console.log(response);
-          if (response.data?.login.errors) {
-            setErrors(toErrorMap(response.data.login.errors));
-          } else if (response.data?.login.user) {
-            // Register successful
-            router.push('/');
-          }
-        }}
-      >
+        onSubmit={handleSubmit}>
         {({ isSubmitting }) => (
           <Form>
             <InputField
@@ -40,7 +45,15 @@ const Login: React.FC = () => {
                 label='Password'
               />
             </Box>
-            <Button type='submit' color='white' bgColor='teal' isLoading={isSubmitting} mt={4}>Login</Button>
+            <Button
+              type='submit'
+              color='white'
+              bgColor='teal'
+              isLoading={isSubmitting}
+              disabled={isSubmitting}
+              mt={4}>
+              Login
+            </Button>
           </Form>
         )}
       </Formik>
