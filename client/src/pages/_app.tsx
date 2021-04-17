9a684ad6f469a8f517501c3e8cfb0815ecbeb51b
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-16 23:08:49 -0400

    add font weights

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import '@fontsource/ibm-plex-sans';
+import '@fontsource/ibm-plex-sans/500.css';
+import '@fontsource/ibm-plex-sans/700.css';
 import '@fontsource/noto-sans';
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 import { Provider, createClient } from 'urql';
@@ -19,8 +21,7 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
         <ColorModeProvider
           options={{
             useSystemColorMode: true,
-          }}
-        >
+          }}>
           <Component {...pageProps} />
         </ColorModeProvider>
       </ChakraProvider>
