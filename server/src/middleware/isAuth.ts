30e84cddcfd50f382f4619408f108b0ea9ae4f7b
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-20 22:49:44 -0400

    change error msg

--- server/src/middleware/isAuth.ts
+++ server/src/middleware/isAuth.ts
@@ -1,9 +1,17 @@
-import { MyContext } from '../types';
 import { MiddlewareFn } from 'type-graphql';
+import { MyContext } from '../types';
+// import { FieldError } from '../utils/FieldError';
 
 export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
   if (!context.req.session.userId) {
-    throw new Error('User not authenticated');
+    throw new Error('Not authenticated');
+
+    // return [
+    //   {
+    //     field: 'api',
+    //     message: 'Not authenticated',
+    //   },
+    // ];
   }
 
   return next();
