89dddf707c3a62b0ababdea61233cdaaf0585bca
Ethan Bonsignori <ebonsignori@gmail.com>
2021-04-20 22:47:37 -0400

    add pagination

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,25 +1,75 @@
-import { Stack } from '@chakra-ui/layout';
+import { Box, Flex, Heading, Link, Stack, Text } from '@chakra-ui/layout';
 import { useColorModeValue } from '@chakra-ui/color-mode';
 import { Navbar } from '../components/Navbar';
 import { withUrqlClient } from 'next-urql';
 import { createUrqlClient } from '../utils/createUrqlClient';
 import { usePostsQuery } from '../generated/graphql';
+import NextLink from 'next/link';
+import { Button } from '@chakra-ui/button';
+import { Spinner } from '@chakra-ui/spinner';
+import { useState } from 'react';
 
 const Index: React.FC = () => {
   const color = useColorModeValue('lightText', 'darkText');
   const bg = useColorModeValue('lightBg', 'darkBg');
 
-  const [{ data }] = usePostsQuery();
+  const [queryVars, setQueryVars] = useState({
+    limit: 10,
+    cursor: null as null | string,
+  });
+  const [{ data, fetching }] = usePostsQuery({
+    variables: queryVars,
+  });
+
+  if (!fetching && !data) {
+    // TODO: error banner
+    return <div>Could not get posts</div>;
+  }
 
   return (
-    <Stack as='main' align='center' bg={bg} color={color} minH='calc(100vh)'>
+    <Stack
+      as='main'
+      align='center'
+      bg={bg}
+      color={color}
+      minH='calc(100vh)'
+      pb={10}>
       <Navbar />
-      <div>Hello World</div>
-      <br />
-      {!data ? (
-        <div>loading...</div>
+      <Flex align='center' justifyContent='space-between'>
+        <Heading>cReddit</Heading>
+        <NextLink href='/create-post' passHref>
+          <Link ml={10} as={Button}>
+            Create Post
+          </Link>
+        </NextLink>
+      </Flex>
+      {!data && fetching ? (
+        <Box my='2em !important'>
+          <Spinner color={color} size='xl' />
+        </Box>
       ) : (
-        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+        <Stack spacing={4}>
+          {data!.posts.posts.map((post) => (
+            <Box p={5} shadow='md' key={post.id} borderWidth='1px' bg='white'>
+              <Heading fontSize='xl'>{post.title}</Heading>
+              <Text mt={4}>{post.textSnippet}</Text>
+            </Box>
+          ))}
+        </Stack>
+      )}
+      {data && data.posts.hasMore && (
+        <Button
+          isLoading={fetching}
+          m='auto'
+          mt={5}
+          onClick={() => {
+            setQueryVars({
+              limit: queryVars.limit,
+              cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
+            });
+          }}>
+          Load More
+        </Button>
       )}
     </Stack>
   );
--- client/src/utils/createUrqlClient.ts
+++ client/src/utils/createUrqlClient.ts
@@ -1,5 +1,5 @@
-import { dedupExchange } from '@urql/core';
-import { cacheExchange } from '@urql/exchange-graphcache';
+import { dedupExchange, stringifyVariables } from '@urql/core';
+import { cacheExchange, Resolver } from '@urql/exchange-graphcache';
 import { fetchExchange } from 'urql';
 import {
   LoginMutation,
@@ -10,6 +10,42 @@ import {
 } from '../generated/graphql';
 import { betterUpdateQuery } from './betterUpdateQuery';
 
+const cursorPagination = (): Resolver => {
+  return (_parent, fieldArgs, cache, info) => {
+    const { parentKey: entityKey, fieldName } = info;
+    const allFields = cache.inspectFields(entityKey);
+    const fieldInfos = allFields.filter((info) => info.fieldName === fieldName);
+    const size = fieldInfos.length;
+    if (size === 0) {
+      return undefined;
+    }
+
+    const fieldKey = `${fieldName}(${stringifyVariables(fieldArgs)})`;
+    const isItInTheCache = cache.resolve(
+      cache.resolveFieldByKey(entityKey, fieldKey) as string,
+      'posts',
+    );
+    info.partial = !isItInTheCache;
+    let hasMore = true;
+    const results: string[] = [];
+    fieldInfos.forEach((fi) => {
+      const key = cache.resolveFieldByKey(entityKey, fi.fieldKey) as string;
+      const data = cache.resolve(key, 'posts') as string[];
+      const _hasMore = cache.resolve(key, 'hasMore');
+      if (!_hasMore) {
+        hasMore = _hasMore as boolean;
+      }
+      results.push(...data);
+    });
+
+    return {
+      __typename: 'PaginatedPosts',
+      hasMore,
+      posts: results,
+    };
+  };
+};
+
 export const createUrqlClient = (ssrExchange: any) => ({
   url: 'http://localhost:4000/graphql',
   fetchOptions: {
@@ -18,6 +54,14 @@ export const createUrqlClient = (ssrExchange: any) => ({
   exchanges: [
     dedupExchange,
     cacheExchange({
+      keys: {
+        PaginatedPosts: () => null,
+      },
+      resolvers: {
+        Query: {
+          posts: cursorPagination(),
+        },
+      },
       updates: {
         Mutation: {
           login: (_result, args, cache, info) => {
